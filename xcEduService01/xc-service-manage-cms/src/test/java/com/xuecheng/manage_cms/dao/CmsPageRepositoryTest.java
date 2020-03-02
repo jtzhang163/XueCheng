f64f70a872f604bae453d499cc8364e039f2f18c
ZhangJianTong <jtzhang163@163.com>
2020-03-02 16:20:11 +0800

    完成页面查询服务端开发（Dao，Service，Controller）

---
+++ xcEduService01/xc-service-manage-cms/src/test/java/com/xuecheng/manage_cms/dao/CmsPageRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.xuecheng.manage_cms.dao;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class CmsPageRepositoryTest {
+
+    @Autowired
+    private CmsPageRepository cmsPageRepository;
+
+    @Test
+    public void testFindPage() {
+        int page = 0;//从0开始
+        int size = 10;//每页记录数
+        Pageable pageable = PageRequest.of(page, size);
+        Page<CmsPage> all = cmsPageRepository.findAll(pageable);
+        System.out.println(all);
+    }
+
+    @Test
+    public void testUpdate() {
+        Optional<CmsPage> optional = cmsPageRepository.findById("5b17a34211fe5e2ee8c116c9");
+        if(optional.isPresent()){
+            CmsPage cmsPage = optional.get();
+            cmsPage.setPageName("测试页面01");
+            cmsPageRepository.save(cmsPage);
+        }
+    }
+
+}
