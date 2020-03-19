a79c23bb3645c22737be755c9768f3472bc00f47
ZhangJianTong <jtzhang163@163.com>
2020-03-19 09:50:50 +0800

    Oauth2授权码模式

--- xcEduService01/xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/config/AuthorizationServerConfig.java
+++ xcEduService01/xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/config/AuthorizationServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.bootstrap.encrypt.KeyProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -136,6 +137,16 @@ class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
     }
 
 
-
+    //Http安全配置，对每个到达系统的http请求链接进行校验
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        //所有请求必须认证通过
+        http.authorizeRequests()
+                //下边的路径放行
+                .antMatchers("/v2/api-docs", "/swagger-resources/configuration/ui",
+                        "/swagger-resources","/swagger-resources/configuration/security",
+                        "/swagger-ui.html","/webjars/**").permitAll()
+                .anyRequest().authenticated();
+    }
 }
 
